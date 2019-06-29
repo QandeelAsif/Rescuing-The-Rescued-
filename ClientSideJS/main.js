bb83f13b64e27cd2ede17e00a7be7567380103f4
Javeria <javerianadeem03@gmail.com>
2019-06-29 22:34:20 +0500

    fixed carousel

---
+++ ClientSideJS/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    M.Carousel.init(elems);
+});
--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 //importing packages
-var express =    require("express"),
-    logger  =    require("morgan"),
-    bodyParser=  require("body-parser");
+var express = require("express"),
+    logger = require("morgan"),
+    bodyParser = require("body-parser");
 
 //creating instance of express app
-var app=express();
+var app = express();
 
 //seting things up
 app.use(logger('dev'));
-app.set('view engine','ejs');
-    //to include static files everytime the page loads
+app.set('view engine', 'ejs');
+//to include static files everytime the page loads
 app.use(express.static('views'));
-app.set('views',__dirname+'/views');
-    //to get posted data in json format
+app.set('views', __dirname + '/views');
+//to get posted data in json format
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
 //ROUTES
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.render("home");
 })
 
-app.post("/",function(req,res){
-   
+app.post("/", function (req, res) {
+
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function (req, res) {
     res.render("login");
 })
 
 //LISTENER PORT
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server started");
 });
